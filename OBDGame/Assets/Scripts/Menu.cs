10ee412a7fa7ce1d37f779e188db8f72c58a51d8
nikita alexeev <nakinaki243@gmail.com>
2023-04-30 19:23:13 +0500

    Created Menu (0-1)
    Нереализованные две кнопки "начать игру" и "выбор уровня"

---
+++ OBDGame/Assets/Scripts/Menu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public void PlayCurLevel()
+    {
+
+    }
+
+    public void Exit()
+    {
+        Debug.Log("exit");
+        Application.Quit();
+    }
+}
---
+++ OBDGame/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    public Slider sliderValueSound;
+    public AudioSource audio;
+    public float volume;
+
+    private void Start()
+    {
+        Load();
+        ValueSound();
+    }
+
+    public void SliderSound()
+    {
+        volume = sliderValueSound.value;
+        ValueSound();
+    }
+
+    private void ValueSound()
+    {
+        audio.volume = volume;
+        sliderValueSound.value = volume;
+    }
+
+    public void Save()
+    {
+        PlayerPrefs.SetFloat("volume", volume);
+    }
+
+    private void Load()
+    {
+        volume = PlayerPrefs.GetFloat("volume");
+    }
+}
